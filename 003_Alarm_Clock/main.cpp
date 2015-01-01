a5b4ac8eecca1cedb6f488ec93ad8a418ba67664
Dmytro Frolov <dimaf@i.ua>
2015-01-01 22:34:24 +0200

    New Program - alarm

---
+++ 003_Alarm_Clock/main.cpp
@@ -0,0 +1,12 @@
+//Alarm Clock - A simple clock where it
+//plays a sound after X number of minutes/seconds or at a particular time.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello, World!" << endl;
+    return 0;
+}
