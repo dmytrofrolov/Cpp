9c7e015103b98571b65bf76f9fe4c4faac5d0f25
Dmytro Frolov <dimaf@i.ua>
2015-01-01 22:51:21 +0200

    Some edit

--- 003_Alarm_Clock/main.cpp
+++ 003_Alarm_Clock/main.cpp
@@ -6,7 +6,15 @@
 using namespace std;
 
 int main() {
+    cout << "Timer - 1 \n Alarm - 2 \n Input : ";
+    int choice = 0;
+    cin >> choice;
+    if(choice == 1){
+        
+    }
+    else if(choice == 2){
 
+    }
     cout << "Hello, World!" << endl;
     return 0;
 }
