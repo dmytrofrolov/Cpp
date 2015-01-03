ae56a973daa14f6ff2c902e60a361065625190c0
Dmytro Frolov <dimaf@i.ua>
2015-01-03 18:23:19 +0200

    Add new project 005_Distance_Between_Two_Cities

---
+++ 005_Distance_Between_Two_Cities/README.md
@@ -0,0 +1,5 @@
+**Distance Between Two Cities** - Calculates the distance between two 
+cities and allows the user to specify a unit of distance. 
+This program may require finding coordinates for the cities like latitude and longitude.
+
+Taken from Karan project list
---
+++ 005_Distance_Between_Two_Cities/main.cpp
@@ -0,0 +1,12 @@
+// Distance Between Two Cities - Calculates the distance between
+// two cities and allows the user to specify a unit of distance.
+// This program may require finding coordinates for the cities like latitude and longitude.
+// Dmytro Frolov
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
