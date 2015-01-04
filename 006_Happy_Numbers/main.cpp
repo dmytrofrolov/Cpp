b1eca0ec3bd37d9ab3ac74139790ef5c989b5afe
Dmytro Frolov <dimaf@i.ua>
2015-01-04 21:58:59 +0200

    Edit 006_Happy_Numbers/main.cpp

--- 006_Happy_Numbers/main.cpp
+++ 006_Happy_Numbers/main.cpp
@@ -6,10 +6,67 @@
 // of your output here. Find first 8 happy numbers.
 //Dmytro Frolov
 #include <iostream>
-
+#include <set>
 using namespace std;
 
+unsigned int digitSquareSum(unsigned int number);
+bool isHappySimple(unsigned int number);
+bool isHappyWithSet(unsigned int number);
+
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Input number : " << endl;
+    unsigned int number = 0;
+    cin >> number;
+    cout << "Simple method - " <<(isHappySimple(number)?"Number is Happy":"Number is unhappy") << endl;
+    cout << "Method with set - " <<(isHappyWithSet(number)?"Number is Happy":"Number is unhappy") << endl;
+    cout << endl;
+    unsigned short int happyNumbersCounter = 0;
+    unsigned int happyNumber = 1;
+    while(happyNumbersCounter<8){
+        if(isHappyWithSet(happyNumber)){
+            cout << "Happy number #" << happyNumbersCounter+1 << " is " << happyNumber << endl;
+            cout.flush();
+            happyNumbersCounter++;
+        }
+        happyNumber++;
+
+    }
     return 0;
+}
+
+
+
+
+bool isHappyWithSet(unsigned int number) {
+    bool isHappy = false;
+    set<unsigned int> digitSquareSums;
+    set<unsigned int>::iterator findPos;
+    unsigned int currentSum = 0;
+    do{
+        currentSum = digitSquareSum(number);
+        findPos = digitSquareSums.find(currentSum);
+        digitSquareSums.insert(currentSum);
+        if(currentSum==1)isHappy = true;
+        number = currentSum;
+    }while(isHappy!=true && findPos==digitSquareSums.end());
+    return isHappy;
+}
+
+bool isHappySimple(unsigned int number) {
+    unsigned int currentSum = 0;
+    do{
+        currentSum = digitSquareSum(number);
+        number = currentSum;
+        if(currentSum==1)return true;
+        else if (currentSum>1 && currentSum<10)return false;
+    }while(1);
+}
+
+unsigned int digitSquareSum(unsigned int number) {
+    unsigned int tempSum = 0;
+    while(number>0){
+        tempSum+=(number%10)*(number%10);
+        number/=10;
+    }
+    return tempSum;
 }
