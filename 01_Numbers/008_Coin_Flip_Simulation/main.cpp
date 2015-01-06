59ddd04fac9c284593c8d2c4ba02ba98f6baa4de
Dmytro Frolov <dimaf@i.ua>
2015-01-06 13:38:00 +0200

    Add README.md

--- 01_Numbers/008_Coin_Flip_Simulation/main.cpp
+++ 01_Numbers/008_Coin_Flip_Simulation/main.cpp
@@ -1,5 +1,10 @@
-#include <iostream>
+//Coin Flip Simulation - Write some code that simulates flipping a
+// single coin however many times the user decides. The code should
+// record the outcomes and count the number of tails and heads.
+// Dmytro Frolov
 
+#include <iostream>
+//#include <>
 using namespace std;
 
 int main() {
