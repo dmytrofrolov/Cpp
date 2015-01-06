13857d94c6ae13c90ae2a02d2b7be09f693f6498
Dmytro Frolov <dimaf@i.ua>
2015-01-06 14:45:01 +0200

    Edit 01_Numbers/008_Coin_Flip_Simulation project

--- 01_Numbers/008_Coin_Flip_Simulation/main.cpp
+++ 01_Numbers/008_Coin_Flip_Simulation/main.cpp
@@ -4,10 +4,42 @@
 // Dmytro Frolov
 
 #include <iostream>
-//#include <>
+#include <windows.h>//for Sleep and rand
+#include <conio.h>  //for keys
+#include <ctime>    //for time
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int input = 0, first = 0, second = 0;
+    cout << "Press any key to flip the coin..." << endl;
+    getch();
+    srand(time(0));
+    do{
+        for(int i = 3; i>0; i--){
+            cout << i << " ";
+            cout.flush();
+            Sleep(700);
+        }
+        for(int i = 3; i>0; i--) {
+            cout << ".";
+            cout.flush();
+            Sleep(500);
+        }
+        cout << endl;
+        int coin = rand()%2;
+        if(coin==1){
+            cout << "\tFirst side!" << endl;
+            first++;
+        }
+        else {
+            cout << "\tSecond side!" << endl;
+            second++;
+        }
+        cout << "Press any key to continue, ESC to exit:" << endl;
+        input = getch();
+    }while(input!=27);
+    cout << "Total:\nFirst side:\t" << first << "\tSecond side:\t" << second << endl;
+    cout << "Press any key to exit..." << endl;
+    getch();
     return 0;
 }
