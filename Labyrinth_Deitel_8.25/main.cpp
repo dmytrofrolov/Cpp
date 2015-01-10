c9542dcf300c0570fe35f32fba80bee58497282b
Dmytro Frolov <dimaf@i.ua>
2015-01-10 16:18:23 +0200

    Some edition of Labyrinth_Deitel_8.25

--- Labyrinth_Deitel_8.25/main.cpp
+++ Labyrinth_Deitel_8.25/main.cpp
@@ -1,34 +1,40 @@
 //Labyrinth task
 //Dmytro Frolov
 #include <iostream>
-
-#ifndef NOLOGGING
-#define LOG(msg)
-#else
-#define LOG(msg) std::cout << "[LOG] (" __FILE__ ":" << __LINE__ << ") from " << __func__ << "()\n    " << msg << "\n";
-#endif
-
+#include <fstream>
 using namespace std;
-void mazeTraverse(char *maze, int size, int startingPoint[2]);
+void mazeTraverse(char **maze, int size, int startingPoint[2]);
 int main() {
-    char *maze[] = {
-             "####",
-             "#...",
-             "#..#",
-             "##.#"
-    };
-    int startingPoint[2] = {1,3};
-    mazeTraverse( maze, 4, startingPoint);
-    cout << "Hello, World!" << endl;
+    fstream labyrinth;
+    labyrinth.open("labyrinth_1.txt");
+    if(!labyrinth.is_open()){
+        cout << "ERROR! With file opening" << endl;
+        return -1;
+    }
+    char row[100];
+    char **maze;
+    maze = new char *[20];
+    for(int i = 0; i <20; i++)
+        maze[i] = new char[20];
+    int j = 0;
+    while(!labyrinth.eof()){
+        labyrinth >> row;
+        for(int i = 0; i<12; i++)
+            maze[j][i] = row[i];
+        j++;
+    }
+
+
+    int startingPoint[2] = {4,11};
+    mazeTraverse( maze, 12, startingPoint);
+    labyrinth.close();
     return 0;
 }
 
 
 
-void mazeTraverse(char *maze, int size, int startingPoint[2]) {
-    //maze[startingPoint[0]][startingPoint[1]] = 'T';
-    cout << startingPoint[0] << " " << startingPoint[1] << endl;
-    cout << maze[startingPoint[0]][startingPoint[1]]  << endl;
+void mazeTraverse(char **maze, int size, int startingPoint[2]) {
+    
     for(int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++)
             cout << maze[i][j] << " ";
