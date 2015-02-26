ba27a018c33aec681eb3603b1ac16508c8e811cf
Dmytro Frolov <dimaf@i.ua>
2015-02-26 22:44:03 +0200

    Add some files

---
+++ HTTP_Server/01_simple_boost_http_server/main.cpp
@@ -0,0 +1,19 @@
+/*
+    This is the simple Boost HTTP Server
+    using Boost.Asio
+    Creation date: Feb 26, 2015
+    Dmytro Frolov
+*/
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
+
+//this compiles successfully =) don't forget -lboost_system flag
