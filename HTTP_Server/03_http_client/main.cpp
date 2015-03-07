0b7b06178a218d5d6f60b6b15fce758b135894c8
Dmytro Frolov <dimaf@i.ua>
2015-03-07 22:35:00 +0200

    some edition http client

--- HTTP_Server/03_http_client/main.cpp
+++ HTTP_Server/03_http_client/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <boost/asio.hpp>
 using namespace std;
+using boost::asio::ip::tcp;
 
-int main()
+int main(int argc, char* argv[])
 {
-    //this is the client for testing servers
+    try
+    {
+        //all boost programs should have at least one io service
+        boost::asio::io_service io_service;
+
+
+
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
     return 0;
 }
